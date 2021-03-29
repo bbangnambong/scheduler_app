802c719af804f07ee8790cda261502928a09bda4
박찬 <parkchan@bagchan-ui-MacBookAir.local>
2021-03-29 22:25:57 +0900

    add bottom buttons

---
+++ lib/root_app.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+
+class RootApp extends StatefulWidget {
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: basicMenu(),
+    );
+  }
+
+  Widget basicMenu() {
+    List<Widget> basic_menus = [
+      GFIconButton(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        size: GFSize.LARGE,
+        icon: Icon(Icons.add),
+        onPressed: () {
+          setState(() {});
+        },
+        color: GFColors.PRIMARY,
+        shape: GFIconButtonShape.pills,
+      ),
+      SizedBox(
+        width: 20,
+      ),
+      Expanded(
+        child: GFButton(
+          text: "리스트",
+          textStyle: TextStyle(fontSize: 17),
+          onPressed: () {},
+          size: GFSize.LARGE,
+          color: GFColors.WARNING,
+          shape: GFButtonShape.pills,
+        ),
+      ),
+      SizedBox(
+        width: 20,
+      ),
+      Expanded(
+        child: GFButton(
+          text: "공유하기",
+          textStyle: TextStyle(fontSize: 17),
+          onPressed: () {},
+          size: GFSize.LARGE,
+          color: GFColors.WARNING,
+          shape: GFButtonShape.pills,
+        ),
+      ),
+      SizedBox(
+        width: 20,
+      ),
+      GFIconButton(
+        icon: Icon(Icons.settings),
+        onPressed: () {},
+        color: GFColors.SUCCESS,
+        shape: GFIconButtonShape.pills,
+      )
+    ];
+
+    return Container(
+      width: double.infinity,
+      height: 80,
+      decoration: BoxDecoration(color: Colors.transparent),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 30, right: 30, bottom: 10),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: basic_menus),
+      ),
+    );
+  }
+}
